488d4b0019ba7a19bdaf5e51b3dd305091eb29d7
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2022-08-11 20:54:44 -0500

    dev get w _id

--- routes/api.js
+++ routes/api.js
@@ -14,19 +14,22 @@ const mongoose = require('mongoose'),
         title: {type: String, required: true},
         comments: [String],
         commentcount: Number
-      });
+      }),
+      Book = mongoose.model('Book', book);
 
 module.exports = function (app) {
 
   app.route('/api/books')
     .get(function (req, res){
+      Book.find({}, (err, docs) => {
+        err ? res.send('no book exists') : res.json(docs);
+      });
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
     })
     
     .post(function (req, res){
       let title = req.body.title,
-          Book = mongoose.model('Book', book),
           newBook = new Book({
             title: title,
             comments: [],
@@ -47,12 +50,23 @@ module.exports = function (app) {
   app.route('/api/books/:id')
     .get(function (req, res){
       let bookid = req.params.id;
+      if (!bookid) {
+        res.send('_id missing');
+      } else {
+        Book.findOne({_id: bookid}, (err, doc) => {
+          console.log('get w/_id result', bookid, err, doc);
+          (err || doc === null) ? res.send('no book exists') : res.json({_id: bookid, title: doc.title, comments: doc.comments});
+        });
+      }
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
     
     .post(function(req, res){
       let bookid = req.params.id;
       let comment = req.body.comment;
+      Book.find({_id: bookid}, (err, doc) => {
+
+      });
       //json res format same as .get
     })
     
